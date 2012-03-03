dee7c5a39228d78c43462fcd3b9b561b3c8c05cf
Joe Snikeris <joe@snikeris.com>
2012-03-02 23:05:16 -0500

    disable tool-bar and menu-bar

--- config/custom.el
+++ config/custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(iswitchb-mode t)
- '(column-number-mode t))
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
