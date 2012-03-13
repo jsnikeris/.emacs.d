631c481b3baafa371042bc4586114b4d5e8fcc79
Joe Snikeris <joe@snikeris.com>
2012-03-13 10:40:40 -0400

    various core emacs customizations

--- config/custom.el
+++ config/custom.el
@@ -4,9 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(indent-tabs-mode nil)
  '(iswitchb-mode t)
  '(menu-bar-mode nil)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
