c28a78022c9457ae2ba81edbb6997f1651e1bc90
Joe Snikeris <joe@snikeris.com>
2012-03-17 14:41:46 -0400

    remove malabar-mode config

--- config/my-cedet.el
+++
@@ -1,7 +0,0 @@
-(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
-				  global-semanticdb-minor-mode
-				  global-semantic-idle-summary-mode
-				  global-semantic-mru-bookmark-mode))
-(semantic-mode 1)
-
-(provide 'my-cedet)
