9e0f6b059c157206539548b1adbfc016cc53546d
Joe Snikeris <joe@snikeris.com>
2012-03-17 14:50:00 -0400

    use expand-file-name rather than concat

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
 (defvar my-init-dir (file-name-directory load-file-name)
   "The directory under which Emacs' initialization files reside.")
 
-(defvar my-config-dir (concat my-init-dir "config/")
+(defvar my-config-dir (expand-file-name "config" my-init-dir)
   "The directory under which configuration for various packages resides.")
 
-(load (setq custom-file (concat my-config-dir "custom.el")))
+(load (setq custom-file (expand-file-name "custom.el" my-config-dir)))
 
 (add-to-list 'load-path my-config-dir)
 (mapc (lambda (filename)
