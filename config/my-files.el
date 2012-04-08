ee15ba0bad5048427d2f8e9727e5cfc7c539e478
Joe Snikeris <joe@snikeris.com>
2012-04-08 09:08:03 -0400

    no backup files when editing w/ sudo

---
+++ config/my-files.el
@@ -0,0 +1,9 @@
+(setq backup-enable-predicate
+      (lambda (name)
+        (and (normal-backup-enable-predicate name)
+             (not
+              (let ((method (file-remote-p name 'method)))
+                (when (stringp method)
+                  (member method '("su" "sudo"))))))))
+
+(provide 'my-files)
