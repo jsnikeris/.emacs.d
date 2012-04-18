f884692a1adab65655c8fe5448856a6d8ff51245
Joe Snikeris <joe@snikeris.com>
2012-04-18 14:49:18 -0400

    add ability to view JVM-provided sources

--- config/my-malabar.el
+++ config/my-malabar.el
@@ -6,6 +6,8 @@
   (setq malabar-groovy-lib-dir (expand-file-name "target/lib" my-malabar-dir))
   (setq malabar-groovy-extra-classpath
         (list (expand-file-name "target/classes" my-malabar-dir)))
+  (setq malabar-extra-source-locations
+        '("/usr/lib/jvm/java-6-sun-1.6.0.26/src.zip"))
   (add-to-list 'load-path (expand-file-name "src/main/lisp" my-malabar-dir))
   (require 'malabar-mode)
   (add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode)))
