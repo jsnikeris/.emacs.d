803196fcd6cbc166de3f8979ef2cf20c8baaf8e5
Joe Snikeris <joe@snikeris.com>
2012-06-13 00:48:05 -0400

    disable scroll-bar-mode

--- config/custom.el
+++ config/custom.el
@@ -1,18 +1,19 @@
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(indent-tabs-mode nil)
  '(iswitchb-mode t)
  '(menu-bar-mode nil)
  '(require-final-newline t)
+ '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
  '(visible-bell t))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  )
