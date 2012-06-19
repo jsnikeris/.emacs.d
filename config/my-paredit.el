b53a547830e070d3917a897091946214abb9a3f3
Joe Snikeris <joe@snikeris.com>
2012-06-18 22:44:54 -0400

    add keybindings for unbound paredit-wrap fns

--- config/my-paredit.el
+++ config/my-paredit.el
@@ -5,4 +5,9 @@
 (autoload 'paredit-mode "paredit"
   "Minor mode for pseudo-structurally editing Lisp code." t)
 
+(eval-after-load "paredit"
+  '(progn
+     (define-key paredit-mode-map (kbd "M-[") 'paredit-wrap-square)
+     (define-key paredit-mode-map (kbd "M-{") 'paredit-wrap-curly)))
+
 (provide 'my-paredit)
