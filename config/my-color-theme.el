be17e834f0a1f5c1aad2960dd78c93422eff0a67
Joe Snikeris <joe@snikeris.com>
2012-06-18 22:44:08 -0400

    change color-theme depending on time of day

--- config/my-color-theme.el
+++ config/my-color-theme.el
@@ -1,7 +1,14 @@
 (require 'my-package)
+(require 'theme-changer)
 
 (my-package-install-package 'color-theme)
 (my-package-install-package 'color-theme-solarized)
-(color-theme-solarized-dark)
+
+(require 'color-theme-solarized)
+
+;; set calendar-location-name, calendar-latitude, and calendar-longitude
+;; in site-start.el (e.g. /etc/emacs/site-start.el)
+(when calendar-latitude
+  (change-theme 'color-theme-solarized-light 'color-theme-solarized-dark))
 
 (provide 'my-color-theme)
