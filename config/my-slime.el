7b0ffd6274a8058b34cc6e9cb5c7520c0e650c49
Joe Snikeris <joe@snikeris.com>
2012-07-08 23:34:08 -0400

    on startup, connect to swank server if running

--- config/my-slime.el
+++ config/my-slime.el
@@ -1,10 +1,17 @@
-(defun slime ()
+(defvar my-slime-port 4005)
+
+(defun my-slime-load ()
   (interactive)
-  (fmakunbound 'slime)
   (load (expand-file-name "~/.quicklisp/slime-helper.el"))
-  (set-language-environment "UTF-8")
-  (setq slime-net-coding-system 'utf-8-unix)
-  (setq inferior-lisp-program "/usr/bin/sbcl")
-  (slime))
+  (setq inferior-lisp-program "sbcl")
+  (require 'slime))
+
+(make-network-process
+ :name "swank-ping" :host 'local :service my-slime-port :nowait t
+ :sentinel (lambda (proc status)
+             (delete-process proc)
+             (when (string-match "open" status)
+               (my-slime-load)
+               (slime-connect 'local my-slime-port))))
 
 (provide 'my-slime)
