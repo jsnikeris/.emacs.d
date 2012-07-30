3e8a6be02e04842b7218d68dc643ccec91264079
Joe Snikeris <joe@snikeris.com>
2012-07-30 10:24:51 -0400

    make M-. find source of thing at point in emacs-lisp-mode

---
+++ config/my-emacs-lisp.el
@@ -0,0 +1,11 @@
+(require 'lisp-mode)
+
+(defun my-find-thing-at-point ()
+  (interactive)
+  (if (not (equal (variable-at-point) 0))
+      (find-variable-at-point)
+    (find-function-at-point)))
+
+(define-key emacs-lisp-mode-map (kbd "M-.") 'my-find-thing-at-point)
+
+(provide 'my-emacs-lisp)
