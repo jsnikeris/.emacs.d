1ff9791bbe8c896fafbc17c9ad73f283193cb413
Joe Snikeris <joe@snikeris.com>
2012-07-30 10:23:56 -0400

    use clojure-mode from ~/src if found

--- config/my-clojure-mode.el
+++ config/my-clojure-mode.el
@@ -1,8 +1,12 @@
 (require 'my-package)
 (require 'my-paredit)
 
-(my-package-install-package 'clojure-mode)
+(let ((src-dir (expand-file-name "~/src/clojure-mode")))
+  (if (file-readable-p src-dir)
+      (add-to-list 'load-path src-dir)
+    (my-package-install-package 'clojure-mode)))
 
+(require 'clojure-mode)
 (add-hook 'clojure-mode-hook 'paredit-mode)
 
 (provide 'my-clojure-mode)
