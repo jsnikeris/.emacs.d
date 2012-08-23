7c0ed468867a0a928f15931c8764b0f390472834
Joe Snikeris <joe@snikeris.com>
2012-08-23 00:12:54 -0400

    revisit org-mode configuration

--- config/my-keys.el
+++ config/my-keys.el
@@ -2,5 +2,9 @@
 
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 (global-set-key (kbd "C-c s") 'magit-status)
+(global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
 
 (provide 'my-keys)
--- config/my-org.el
+++ config/my-org.el
@@ -1,17 +1,5 @@
 (my-require 'org)
 
-(setq org-agenda-dim-blocked-tasks 'invisible)
-(setq org-agenda-start-on-weekday nil)
-
-(setq org-fast-tag-selection-single-key 'expert)
-(setq org-tag-alist
-      '(("@read" . 114) ("@phone" . 102) ("@computer" . 99) ("@internet" . 105)
-	("@work" . 119) ("@errand" . 101) ("@home" . 104)))
-(setq org-use-tag-inheritance nil)
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)")))
-(setq org-enforce-todo-dependencies t)
-
 (defun my-org-insert-sub-task ()
   (interactive)
   (let ((parent-deadline (org-get-deadline-time nil))
@@ -22,11 +10,66 @@
       (org-deadline nil parent-deadline))
     (when parent-scheduled
       (org-schedule nil parent-scheduled))))
-(define-key org-mode-map (kbd "C-c s") 'my-org-insert-sub-task)
 
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c b") 'org-iswitchb)
+(defun my-remove-empty-drawer-on-clock-out ()
+  "Remove empty LOGBOOK drawers on clock out"
+  (interactive)
+  (save-excursion
+    (beginning-of-line 0)
+    (org-remove-empty-drawer-at "LOGBOOK" (point))))
+
+;;; Hooks
+(add-hook 'org-clock-out-hook 'my-remove-empty-drawer-on-clock-out 'append)
+
+;;; Files
+(setq org-directory (expand-file-name "~/doc"))
+(setq org-agenda-files
+      (list org-directory (expand-file-name "comcast" org-directory)))
+(setq org-default-notes-file (expand-file-name "refile.org" org-directory))
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+
+;;; Agenda
+(setq org-agenda-dim-blocked-tasks 'invisible)
+(setq org-agenda-start-on-weekday nil)
+
+;;; Task States
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
+        (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELED(c@/!)")))
+(setq org-treat-S-cursor-todo-selection-as-state-change nil)
+(setq org-enforce-todo-dependencies t)
+(setq org-todo-state-tags-triggers
+      '(("CANCELLED" ("CANCELLED" . t))
+        ("WAITING" ("WAITING" . t))
+        ("HOLD" ("WAITING" . t) ("HOLD" . t))
+        (done ("WAITING") ("HOLD"))
+        ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+        ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
+        ("DONE" ("WAITING") ("CANCELLED") ("HOLD"))))
+
+;;; Capture
+(setq org-capture-templates
+  '(("t" "todo" entry (file "")
+     "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+    ("r" "respond" entry (file "")
+     "* TODO Respond to %:from on %:subject\n%U\n%a\n"
+     :clock-in t :clock-resume t :immediate-finish t)
+    ("n" "note" entry (file "")
+     "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+    ("j" "Journal" entry
+     (file+datetree (expand-file-name "diary.org" org-directory))
+     "* %?\n%U\n" :clock-in t :clock-resume t)
+    ("h" "Habit" entry (file "")
+     "* NEXT %?\n%U\n%a\nSCHEDULED: %t .+1d/3d\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n")))
+
+;;; Tags
+(setq org-fast-tag-selection-single-key 'expert)
+(setq org-tag-alist
+      '(("@read" . 114) ("@phone" . 102) ("@computer" . 99) ("@internet" . 105)
+	("@work" . 119) ("@errand" . 101) ("@home" . 104)))
+(setq org-use-tag-inheritance nil)
+
+;;; Keybindings
+(define-key org-mode-map (kbd "C-c t") 'my-org-insert-sub-task)
 
 (provide 'my-org)
