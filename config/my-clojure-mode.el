8e1438a14d800df7b9385e60a9f084c23de43ab3
Joe Snikeris <joe@snikeris.com>
2012-08-29 11:38:54 -0400

    enable clojure-mode in *.dtm and *.cljs buffers

--- config/my-clojure-mode.el
+++ config/my-clojure-mode.el
@@ -5,6 +5,7 @@
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 
+(add-to-list 'auto-mode-alist '("\\.\\(clj|cljs|dtm\\)$" . clojure-mode))
 (add-to-list 'whitespace-global-modes 'clojure-mode)
 
 (provide 'my-clojure-mode)
