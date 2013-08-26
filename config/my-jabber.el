859ba6b64e91d1dec1b64a405474783a7bc54fa9
Joe Snikeris <joseph_snikeris@cable.comcast.com>
2013-08-26 16:21:04 -0400

    configure jabber roster display
    - hide offline contacts
    - trim down the default contact display

--- config/my-jabber.el
+++ config/my-jabber.el
@@ -1,11 +1,12 @@
 (my-require 'jabber)
 (my-require 'my-password-cache)         ;password in ~/.secrets.el.gpg
 
-
-(setq jabber-account-list
-      '(("snikeris@gmail.com"
-         (:port . 5223)
-         (:network-server . "173.194.68.125")
-         (:connection-type . ssl))))
+(setq
+ jabber-account-list '(("snikeris@gmail.com"
+                        (:port . 5223)
+                        (:network-server . "173.194.68.125")
+                        (:connection-type . ssl)))
+ jabber-roster-line-format "%c %-25n %u %-8s"
+ jabber-show-offline-contacts nil)
 
 (provide 'my-jabber)
