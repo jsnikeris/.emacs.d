6aec1117812c65f31de093f1fd2148af36e62d88
Joe Snikeris <joe@snikeris.com>
2013-09-03 22:35:44 -0400

    delete directories faster

--- config/my-dired.el
+++ config/my-dired.el
@@ -1,7 +1,8 @@
 (my-require 'dired-x)
 
 (setq-default dired-omit-mode t)
-(setq dired-omit-files
-      (concat dired-omit-files "\\|^\\..+$"))
+(setq
+ dired-omit-files (concat dired-omit-files "\\|^\\..+$")
+ dired-recursive-deletes 'always)
 
 (provide 'my-dired)
