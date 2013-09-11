c2421f3e40dd47a1ff012aef77071f9e08370c67
Joe Snikeris <joe@snikeris.com>
2013-09-11 00:56:01 -0400

    humanize dired's file size display

--- config/my-dired.el
+++ config/my-dired.el
@@ -3,6 +3,7 @@
 (setq-default dired-omit-mode t)
 (setq
  dired-omit-files (concat dired-omit-files "\\|^\\..+$")
+ dired-listing-switches "-alh"
  dired-recursive-deletes 'always)
 
 (provide 'my-dired)
